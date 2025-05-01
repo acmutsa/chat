69473256c8605a152c6c2b252ef7e9ac822ec53a
Liam Murray <liam@murray.to>
2025-05-01 16:46:55 -0500

    adds themeing

---
+++ src/app/(core)/layout.tsx
@@ -0,0 +1,28 @@
+import { db } from "@/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { eq } from "drizzle-orm";
+import { usersTable } from "@/db/schema";
+import { RedirectToSignIn } from "@clerk/nextjs";
+
+export default async function Layout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const { userId } = await auth();
+
+	if (!userId) {
+		return <RedirectToSignIn />;
+	}
+
+	const user = await db.query.usersTable.findFirst({
+		where: eq(usersTable.id, userId),
+	});
+
+	if (!user) {
+		redirect("/onboard");
+	}
+
+	return <div>{children}</div>;
+}
--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -1,6 +1,33 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { db } from "@/db";
+import { usersTable } from "@/db/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { eq } from "drizzle-orm";
+import { Button } from "@/components/ui/button";
+export default async function Page() {
+	const clerkUser = await currentUser();
 
-export default function Page() {
+	if (!clerkUser) {
+		redirect("/sign-in");
+	}
+
+	const user = await db.query.usersTable.findFirst({
+		where: eq(usersTable.id, clerkUser.id),
+	});
+
+	if (!user) {
+		const insertData = {
+			id: clerkUser.id,
+			firstName: clerkUser.firstName ?? "",
+			lastName: clerkUser.lastName ?? "",
+			email: clerkUser.emailAddresses[0].emailAddress,
+			profileImage: clerkUser.hasImage ? clerkUser.imageUrl : "",
+		};
+
+		await db.insert(usersTable).values(insertData);
+	}
+
 	return (
 		<main className="w-full flex-col max-w-screen h-screen flex items-center justify-center">
 			<h1 className="text-7xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground/90 to-foreground/70 pb-5">
@@ -10,6 +37,11 @@ export default function Page() {
 				You are now Registered. Feel free to customize your experience
 				below:
 			</p>
+			<Card className="relative overflow-hidden border-border/40 bg-gradient-to-b from-background to-background/95 backdrop-blur shadow-xl rounded-2xl w-full max-w-[500px] mt-10">
+				<CardContent className="p-0 flex items-center justify-center">
+					<Button>Continue {">"}</Button>
+				</CardContent>
+			</Card>
 		</main>
 	);
 }
