22edb92d3588d61c830ccd4447158388c84ec9d4
Liam Murray <liam@murray.to>
2025-04-30 21:05:23 -0500

    Add prettier

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
-  return (
-    <div>
-      <h1>Onboard</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Onboard</h1>
+		</div>
+	);
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,7 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users_table", {
-  id: int("id").primaryKey({ autoIncrement: true }),
-  name: text("name").notNull(),
-  email: text("email").notNull().unique(),
-  
+	id: int("id").primaryKey({ autoIncrement: true }),
+	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
 });
