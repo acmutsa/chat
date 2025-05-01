880592e5a8ab256e9e6bd1ab20abfc4bc5b4c53c
Liam Murray <liam@murray.to>
2025-05-01 14:57:07 -0500

    Add shadcn theme, fill in onboarding page

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -1,7 +1,16 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Page() {
 	return (
-		<div>
-			<h1>Onboard</h1>
-		</div>
+		<main className="w-full flex-col max-w-screen h-screen flex items-center justify-center">
+			<h1 className="text-7xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground/90 to-foreground/70 pb-5">
+				Welcome!
+			</h1>
+			<p className="font-bold">
+				You are now Registered. Feel free to customize your experience
+				below:
+			</p>
+			<Card></Card>
+		</main>
 	);
 }
